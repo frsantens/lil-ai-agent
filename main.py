c7a7141373ca3a61dc6286b8109add1959124ff1
frsantens <frsantens@gmail.com>
2025-07-02 15:59:15 +0200

    -

--- main.py
+++ main.py
@@ -4,27 +4,29 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
-]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
 
-if len(sys.argv) == 1:
-    print("Usage: python main.py <prompt>")
-    sys.exit(1)
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', 
-    contents=messages
-)
+    if len(sys.argv) == 1:
+        print("Usage: python main.py <prompt> [--verbose]")
+        sys.exit(1)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=messages
+    )
 
-if "--verbose" in sys.argv:
-    print(f"User prompt: {sys.argv[1]}")
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print ("Response tokens:", response.usage_metadata.candidates_token_count)
-print(response.text)
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {sys.argv[1]}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print ("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
